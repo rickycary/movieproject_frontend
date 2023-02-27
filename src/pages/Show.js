517ffeb972ee7227d502c5f6b8bb51ca2d265939
Richard <ricky.cary1@gmail.com>
2023-02-27 17:23:10 -0600

    movieproject: Show page created

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,27 @@
+import { Link, useLoaderData } from "react-router-dom";
+
 const Show = (props) => {
-    return <h1>Show</h1>;
+  const post = useLoaderData()
+
+  // Styles
+  const div = {
+    textAlign: "center",
+    border: "3px solid green",
+    width: "80%",
+    margin: "30px auto",
+  }
+
+    return <div style={div}>
+      <h1>{post.poster}</h1>
+      <h1>{post.movieName}</h1>
+      <h2>{post.genre}</h2>
+      <h2>{post.rating}</h2>
+
+
+      <Link to="/">
+        <button>Go Back</button>
+      </Link>
+    </div>
   };
   
   export default Show;
