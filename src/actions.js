0789e87e93950bede99420be9d249f3f36c83750
Richard <ricky.cary1@gmail.com>
2023-02-27 20:05:29 -0600

    movieproject: Working on action.js

---
+++ src/actions.js
@@ -0,0 +1,23 @@
+import url from "./url";
+import { redirect } from "react-router-dom";
+
+export async function CreateAction({request}){
+    // get the form data
+    const formData = await request.formData()
+
+    // construct new movie
+    const newMovie = {
+        movieName: formData.get("movieName"),
+        genre: formData.get("genre"),
+        rating: formData.get("rating"),
+        poster: formData.get("poster")
+    }
+
+    // request to create route in the backend
+    await fetch(url, {
+        method: "post",
+        headers: {
+            "Content-Type": ""
+        }
+    })
+}
